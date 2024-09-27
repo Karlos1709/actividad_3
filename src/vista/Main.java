643e76ae71b7df8a269bd54d5432786e713c1159
Carlos <csandovals7@miumg.edu.gt>
2024-09-26 19:35:42 -0600

    first commit

---
+++ src/vista/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author carlo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        frm_Docentes frm = new frm_Docentes();
+        frm.show();
+    }
+    
+}
